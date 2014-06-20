a0b6a08e85af57233b56123ee9626678c9b08dac
Roberto Vasquez Angel <robotex@robotex.de>
2014-06-20 14:52:37 +0200

    Enhanced html_safe Fixed missing converions to string on non-string values.

--- app/display_blocks/table_display_block.rb
+++ app/display_blocks/table_display_block.rb
@@ -1,24 +1,24 @@
 class TableDisplayBlock < SimpleDisplay::DisplayBlock::Base
   def display(field, options = {}, &block)
     label = extract_field_label(field)
-    value_prefix + super(field, options, &block) + value_suffix
+    (value_prefix + super(field, options, &block) + value_suffix).html_safe
   end
-  
+
 #  def actions(options = {}, &block)
 #    value_prefix + super(options, &block) + value_suffix
 #  end
 
   def display_action(action, &block)
-    value_prefix + super(action, &block) + value_suffix
+    (value_prefix + super(action, &block) + value_suffix).html_safe
   end
-  
+
   private
-  
+
   def value_prefix
-    "<td>".html_safe
+    "<td>"
   end
-  
+
   def value_suffix
-    "</td>".html_safe
+    "</td>"
   end
 end
